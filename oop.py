3678a419c1d02697a5d433d08f278ae5b2908c97
pramodchahar <pramod.chahar@gmail.com>
2019-04-20 17:32:25 +0530

    push changes

--- oop.py
+++ oop.py
@@ -11,10 +11,13 @@
 # Create en employee class to fill all details 
 
 class Employee: 
+    #Class variables
+    raise_amount=1.04
+    num_of_emps=0
 
     def __init__(self,first,last,age,pay):
 
-
+        
 
         #we can mention what other arguments we want to accept while creating instance of this class
         #set all of the instance variables
@@ -26,11 +29,38 @@ class Employee:
         self.pay=pay
         self.email=first+'.'+last+'@gmail.com'
 
+        Employee.num_of_emps +=1
+
     # add methods to the class  
     def fullName(self):
 
         return ("{} {}" ).format(self.first,self.last)
 
+    def apply_raise(self):
+        self.pay=int(self.pay*self.raise_amount)
+
+    #add class method 
+
+    @classmethod
+    def set_raise_amount(cls,amount):
+        cls.raise_amount=amount
+    #alternative constructors 
+    @classmethod
+
+    def from_string(cls,emp_string):
+       first,last,age,pay=emp_string.split("-")
+       return cls(first,last,age,pay)
+
+    #static method
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday()==5 or day.weekday()==6:
+            return False
+        else:
+            return True
+    
+            
 
 
 
@@ -71,9 +101,63 @@ emp_2=Employee('Neha','Ziaan',31,10)
 #when we create methods within the class ,they recieve the instance(self) as the first argument automatically.You can call that anything apart from self but by convention 'its called 'self')
 
 #Notice we need a parenthesis here as its a method not an attribute
-print(emp_1.fullName())
-print(emp_1.email)
-print(Employee.fullName(emp_1))
+# print(emp_1.fullName())
+# print(emp_1.email)
+# print(Employee.fullName(emp_1))
+
+# print(emp_1.pay)
+# emp_1.apply_raise()
+# print(emp_1.pay)
+#namespace
+# print(emp_1.__dict__)
+#print(Employee.__dict__)
+# print(Employee.num_of_emps)
+
+# Employee.set_raise_amount(1.80)
+# emp_1.set_raise_amount(12.05)
+
+# print(Employee.raise_amount)
+# print(emp_1.raise_amount)
+# print(emp_2.raise_amount)
+
+# emp_3=Employee.from_string('Ziaan-chaudhary-3-66')
+
+# print(emp_3.email)
+
+# print(Employee.num_of_emps)
+
+# import datetime
+
+# my_date=datetime.date(2016,7,10)
+
+# print(Employee.is_workday(my_date))
+
+# Inheritance
+
+class Developer(Employee):
+
+    raise_amount=1.50
+
+    # To add addtional attributes to the subclass , we need to declare init method specific to sublcass 
+
+    def __init__(self,first,last,age,pay,language):
+        #let main class handle default arguments
+
+        super().__init__(first,last,age,pay)
+        #Employee.__init__(self,first,last,age,pay)
+
+        self.languge=languge
+
+    pass
+
+#print(help(Developer))
 
 
+dev_1=Developer('chenna','P',33,5000,'tableau')
+dev_2=Developer('surya','krishna',30,3000,'python')
+#Chnage in the attribute in subclass , it doesnot effect on the main Employee class
 
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
+print(dev_1.languge)
